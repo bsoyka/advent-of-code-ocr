2bba8bc3662a30c292b11572596b7da917d1f3be
Geir Arne Hjelle <geirarne@gmail.com>
2021-12-15 17:30:31 +0100

    Support conversion directly from NumPy arrays

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent of Code® OCR
 
-This Python module helps with converting Advent of Code ASCII art letters into plain characters. At the moment, it only supports 6-pixel-tall characters as seen in 2016 Day 8, 2019 Days 8 and 11, and 2021 Day 13.
+This Python module helps with converting [Advent of Code](https://adventofcode.com/) ASCII art letters into plain characters. At the moment, it only supports 6-pixel-tall characters as seen in 2016 Day 8, 2019 Days 8 and 11, and 2021 Day 13.
 
 Support for 10-pixel-tall characters (2018 Day 10) is coming soon.
 
@@ -35,6 +35,23 @@ print(convert_6(".##.\n#..#\n#..#\n####\n#..#\n#..#"))
 
 print(convert_6(" $$ \n$  $\n$  $\n$$$$\n$  $\n$  $", fill_pixel="$", empty_pixel=" "))
 # A
+```
+
+You can also convert data that you have in a NumPy array or a nested list:
+
+```py
+from advent_of_code_ocr import convert_array_6
+
+array = [
+    [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+    [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
+    [1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+]
+print(convert_array_6(array, fill_pixel=1, empty_pixel=0))
+# AOC
 ```
 
 ---
--- tests/test_6.py
+++ tests/test_6.py
@@ -2,7 +2,7 @@ from re import escape
 
 from pytest import mark, raises
 
-from advent_of_code_ocr import convert_6
+from advent_of_code_ocr import convert_6, convert_array_6
 from advent_of_code_ocr.characters import ALPHABET_6
 
 
@@ -40,7 +40,10 @@ def test_three_letters(test_input, expected):
     ],
 )
 def test_different_characters(test_input, fill_char, empty_char):
-    assert convert_6(test_input, fill_pixel=fill_char, empty_pixel=empty_char) == "ABC"
+    assert (
+        convert_6(test_input, fill_pixel=fill_char, empty_pixel=empty_char)
+        == "ABC"
+    )
 
 
 def test_long_string():
@@ -60,7 +63,9 @@ def test_long_string():
 
 @mark.parametrize("rows", [0, 1, 5, 7, 10])
 def test_number_of_rows(rows):
-    with raises(ValueError, match=escape("incorrect number of rows (expected 6)")):
+    with raises(
+        ValueError, match=escape("incorrect number of rows (expected 6)")
+    ):
         convert_6("\n".join("" for _ in range(rows)))
 
 
@@ -77,3 +82,15 @@ def test_strange_width_characters():
         ]
     )
     assert convert_6(string) == "EFEYKFRFIJ"
+
+
+def test_array_nested_list():
+    array = [
+        [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+        [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
+        [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+    ]
+    assert convert_array_6(array, fill_pixel=1, empty_pixel=0) == "AOC"

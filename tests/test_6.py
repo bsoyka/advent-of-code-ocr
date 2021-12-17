db9af3e6706f8f34b43751ff2bc8c47c6b7e8f59
Geir Arne Hjelle <geirarne@gmail.com>
2021-12-17 16:08:38 +0100

    Add NumPy specific tests

--- tests/test_6.py
+++ tests/test_6.py
@@ -1,5 +1,6 @@
 from re import escape
 
+import numpy as np
 from pytest import mark, raises
 
 from advent_of_code_ocr import convert_6, convert_array_6
@@ -86,11 +87,45 @@ def test_strange_width_characters():
 
 def test_array_nested_list():
     array = [
-        [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
-        [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
-        [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
-        [1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
-        [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
-        [1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+        ["X", "O", "O", "X", "X", "X", "O", "O", "X"],
+        ["O", "X", "X", "O", "X", "O", "X", "X", "O"],
+        ["O", "X", "X", "O", "X", "O", "X", "X", "X"],
+        ["O", "O", "O", "O", "X", "O", "X", "X", "X"],
+        ["O", "X", "X", "O", "X", "O", "X", "X", "O"],
+        ["O", "X", "X", "O", "X", "X", "O", "O", "X"],
     ]
+    assert convert_array_6(array, fill_pixel="O", empty_pixel="X") == "AC"
+
+
+def test_array_list_of_strings():
+    array = [
+        ".oo...oo..oooo..ooo.o..o..ooo..oo..o..o",
+        "o..o.o..o.o....o....o..o...o..o..o.o..o",
+        "o..o.o....ooo..o....oooo...o..o....oooo",
+        "oooo.o....o.....oo..o..o...o..o.oo.o..o",
+        "o..o.o..o.o.......o.o..o...o..o..o.o..o",
+        "o..o..oo..oooo.ooo..o..o..ooo..ooo.o..o",
+    ]
+    assert convert_array_6(array, fill_pixel="o") == "ACESHIGH"
+
+
+def test_array_numpy():
+    array = np.array(
+        [
+            [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+            [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
+            [1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],
+            [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+        ]
+    )
     assert convert_array_6(array, fill_pixel=1, empty_pixel=0) == "AOC"
+
+
+@mark.parametrize("rows", [1, 5, 7, 10])
+def test_array_number_of_rows(rows):
+    with raises(
+        ValueError, match=escape("incorrect number of rows (expected 6)")
+    ):
+        convert_array_6(["" for _ in range(rows)])

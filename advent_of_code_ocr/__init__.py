f87d79b40f2af3f2166955c45a5468578379e990
Ben Soyka <hello@bsoyka.me>
2025-07-26 12:50:21 -0600

    refactor: make version number dynamic
    This makes `pyproject.toml` the single source of truth for the current version number. The module uses the standard `importlib` to pull the version number, and Sphinx now pulls from that variable when building docs.

--- advent_of_code_ocr/__init__.py
+++ advent_of_code_ocr/__init__.py
@@ -1,6 +1,8 @@
 """A library to parse Advent of Code ASCII art."""
 
+from importlib.metadata import version as get_version
+
 from .height_6 import convert_6 as convert_6
 from .height_6 import convert_array_6 as convert_array_6
 
-__version__ = '1.1.0'
+__version__ = get_version(__package__)
